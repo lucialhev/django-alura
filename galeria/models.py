f8968cfde778841997e9b1713094715554a588dd
lucialhev <luciahevicente@gmail.com>
2024-03-14 10:01:26 -0300

    buscar

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from datetime import datetime
+
+class Fotografia(models.Model):
+
+    OPCOES_CATEGORIA = [
+        ("NEBULOSA","Nebulosa"),
+        ("ESTRELA","Estrela"),
+        ("GALÁXIA","Galáxia")
+    ]
+    nome = models.CharField(max_length=100,null=False, blank=False)
+    legenda = models.CharField(max_length=150,null=False, blank=False)
+    categoria = models.CharField(choices = OPCOES_CATEGORIA,max_length=100, default='')
+    descricao = models.TextField(null=False, blank=False)
+    foto = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
+    publicada = models.BooleanField(default=False)
+    data_fotografia = models.DateTimeField(default = datetime.now)
+
+    def __str__(self):
+        return self.nome
